d91eb98e8130dc8520c3d77cc2c0f7ee75791d4b
Mark Tinderholt <mark@vitalsigyn.com>
2020-05-14 18:24:45 -0400

    update secure module outputs

--- topic/secure/outputs.tf
+++ topic/secure/outputs.tf
@@ -1,12 +1,12 @@
 output "endpoint" {
-    value = azurerm_eventgrid_topic.topic.endpoint
+    value = module.topic.endpoint
 }
 output "primary_access_key" {
-    value = azurerm_eventgrid_topic.topic.primary_access_key
+    value = module.topic.primary_access_key
 }
 output "name" {
-    value = "${azurerm_eventgrid_topic.topic.name}"
+    value = module.topic.name
 }
 output "id" {
-    value = "${azurerm_eventgrid_topic.topic.id}"
+    value = module.topic.id
 }
